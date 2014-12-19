b6b7410fe38b6734ce6c0a8b30708bcca8cfd713
Ionică Bizău <bizauionica@yahoo.com>
2014-12-19 08:26:56 +0200

    Get artworks from user

--- test/index.js
+++ test/index.js
@@ -9,5 +9,16 @@ ArtStack.auth(require("./auth"), function (err) {
     ArtStack.tags.list(function (err, artists) {
         if (err) throw err;
         console.log(artists);
+
+        // Check if the user follows anyone
+        if (!artists.length) {
+            return console.log("You don't follow anyone.");
+        }
+
+        // Get artworks from artist
+        ArtStack.artworks.fromArtist(artists[0], function (err, artworks) {
+            if (err) throw err;
+            console.log(artists[0].display_name + " has " + artworks.length + " artworks.");
+        });
     });
 });
